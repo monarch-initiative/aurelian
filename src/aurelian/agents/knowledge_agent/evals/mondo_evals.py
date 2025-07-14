6d2965715049dd63d1aff9f30d4c4ac4f20b506b
Justin Reese <justaddcoffee+github@gmail.com>
2025-07-14 12:06:20 -0400

    Adding back some MONDO test cases

--- src/aurelian/agents/knowledge_agent/evals/mondo_evals.py
+++ src/aurelian/agents/knowledge_agent/evals/mondo_evals.py
@@ -33,11 +33,50 @@ x_linked_cleft_palate_and_ankyloglossia = Case(
     metadata=metadata("hard", "disease_recognition")
 )
 
+congenital_adrenal_hypoplasia = Case(
+    name="congenital adrenal hypoplasia",
+    inputs="Consistent with the idea that cells in this primordium are poised between "
+           "two developmental pathways, some of the genes that are involved in "
+           "establishing sexual dimorphism, including Dax1 (dosage-sensitive sex "
+           "reversal-congenital adrenal hypoplasia critical region on the X chromosome "
+           "protein 1), Sox9 (Sry-like HMG box 9), Fgf9 (fibroblast growth factor 9), "
+           "and Wnt4 (wingless-related MMTV integration site 4), are initially "
+           "expressed in similar patterns in XX and XY gonads [3–8].",
+    expected_output={'ontology_id': "MONDO:0016241"},
+    metadata=metadata("hard", "disease_recognition")
+)
+
+severe_acute_respiratory_syndrome = Case(
+    name="severe acute respiratory syndrome",
+    inputs="Examples of such very successful and useful modeling efforts are provided "
+            "by HIV protease [11], or very recently by the peptidase from a "
+            "coronavirus involved in the severe acute respiratory syndrome [12], "
+            "among others.",
+    expected_output = {'ontology_id': "MONDO:0005091"},
+    metadata = metadata("easy", "disease_recognition")
+)
+
+
+# Mucolipidosis type IV (MLIV) is an autosomal recessive lysosomal storage disorder characterized by severe neurologic and ophthalmologic abnormalities.
+# MONDO:0009653
+# Mucolipidosis type IV
+
+mucolipidosis_type_IV = Case(
+    name="mucolipidosis type IV",
+    inputs="Mucolipidosis type IV (MLIV) is an autosomal recessive lysosomal storage "
+           "disorder characterized by severe neurologic and ophthalmologic "
+           "abnormalities.",
+    expected_output={'ontology_id': "MONDO:0009653"},
+    metadata=metadata("easy", "disease_recognition")
+)
+
 
 def create_mondo_eval_dataset() -> Dataset[str, str, MetadataDict]:
     """Create MONDO disease recognition evaluation dataset from CRAFT corpus."""
     cases = [
-        huntington_disease, movement_disorder, x_linked_cleft_palate_and_ankyloglossia
+        huntington_disease, movement_disorder, x_linked_cleft_palate_and_ankyloglossia,
+        congenital_adrenal_hypoplasia, severe_acute_respiratory_syndrome,
+        mucolipidosis_type_IV
     ]
 
     evaluators = [SimpleEntityEvaluator()]
